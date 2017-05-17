38240ea5314c3e69f463705c956aa008ded47456
U-PERSISTENT\santosh_pawar <santosh_pawar@BHL06728.persistent.co.in>
2017-05-17 16:07:37 +0530

    Added travis CI file

---
+++ spec/default_spec.rb
@@ -0,0 +1,17 @@
+require 'chefspec'
+
+describe 'httpd::default' do
+  let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'centos', version: '7.0').converge(described_recipe) }
+  
+  it "should have default install_method 'package'" do
+    expect(chef_run).to install_package('net-tools')
+  end
+
+  it "should have default install_method 'package'" do
+    expect(chef_run).to install_package('httpd')
+  end
+  
+  it "should have default install_method 'package'" do
+    expect(chef_run).to install_package('apache2')
+  end
+end
