46336cfea29bc97b23a69967984a0d65cac06498
U-PERSISTENT\santosh_pawar <santosh_pawar@BHL06728.persistent.co.in>
2017-05-18 17:38:25 +0530

    Added travis CI file

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,11 +13,11 @@ package 'net-tools' do
 end
 
 package 'install apache' do
-  case node[:platform]
-    when 'redhat', 'centos'
-      package_name 'httpd'
-    when 'ubuntu', 'debian'
-      package_name 'apache2'
-    end
-    action [:install]
+  case node['platform']
+  when 'redhat', 'centos'
+    package_name 'httpd'
+  when 'ubuntu', 'debian'
+    package_name 'apache2'
+  end
+  action [:install]
 end
