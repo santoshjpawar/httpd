56d72837b6a342c69c248e624911b2ad024beddf
U-PERSISTENT\santosh_pawar <santosh_pawar@BHL06728.persistent.co.in>
2017-05-18 18:10:32 +0530

    Added travis CI file

--- metadata.rb
+++ metadata.rb
@@ -2,12 +2,12 @@ name              'httpd'
 maintainer        'Santosh Pawar'
 maintainer_email  'someone@some.mail.com'
 version           '1.0.0'
-chef_version      '>= 12'
+chef_version      '~> 12'
 license           'apachev2'
 description       'Installs/Configures httpd'
 long_description  'Installs/Configures httpd'
 supports          'centos'
 supports          'ubuntu'
-issues_url        'https://github.com/santoshjpawar' if respond_to?(:issues_url)
-source_url        'https://github.com/santoshjpawar' if respond_to?(:source_url)
+issues_url        'https://github.com/santoshjpawar/httpd/issues' if respond_to?(:issues_url)
+source_url        'https://github.com/santoshjpawar/httpd' if respond_to?(:source_url)
 license           'Apache v2.0'
